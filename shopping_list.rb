cd9dfd77c6d9bc2810651a33a7c24a0ff4e12aa2
github username <github email address>
2019-08-19 11:15:00 +0100

    clear button and name input

--- shopping_list.rb
+++ shopping_list.rb
@@ -8,11 +8,25 @@ require "erb"
 class App
 
 	def initialize
-	@tpl = File.read ("./index.erb")
-	@style = File.read ("./stylesheet.css")
-	@db = SQLite3::Database.new("./database.db")
-	@db.execute("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, item TEXT, quantity INTEGER, type TEXT, bought INTEGER);") #database created
+		@tpl = File.read ("./index.erb")
+		@style = File.read ("./stylesheet.css")
+		@db = SQLite3::Database.new("./database.db")
+		@db.execute("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, item TEXT, quantity INTEGER, type TEXT, bought INTEGER);") #database created
+		@db.execute("CREATE TABLE IF NOT EXISTS names (id INTEGER PRIMARY KEY, name TEXT);")
+		@db.execute("SELECT name FROM names LIMIT 1;") do |row|
+			@name = row.first
+		end
 	end
+
+	def insert_name name
+		puts name
+		@db.execute("INSERT INTO names (name) VALUES (?);", name)
+		@name = name
+	end
+
+	# def product_table (item, quantity, type)
+
+	# end
 
 	def call(env)
 		req = Rack::Request.new(env)
@@ -25,11 +39,20 @@ class App
 			return resp.finish
 		end
 
-		if req.env["REQUEST_METHOD"] == "POST" #post writes things onto the database directly from the user input. We're linking it to 
-			item = req.POST["item"].capitalize
-			quantity = req.POST["quantity"].to_i
-			type = req.POST["type"].capitalize
-			@db.execute("INSERT INTO products (item, quantity, type, bought) VALUES (?,?,?,?);", item, quantity, type,  0)
+		if req.env["REQUEST_METHOD"] == "POST" #post writes things onto the database directly from the user input. We're linking it to
+			
+			if req.path == "/name" then insert_name(req.POST["name"]) end
+			if req.path == "/products" then
+				item = req.POST["item"].capitalize
+				quantity = req.POST["quantity"].to_i
+				type = req.POST["type"].capitalize
+				@db.execute("INSERT INTO products (item, quantity, type, bought) VALUES (?,?,?,?);", item, quantity, type,  0) 
+			end
+			if req.path == "/products/clear"
+				clear = req.POST["clear"]
+				@db.execute("DELETE FROM products")
+			end
+
 		end
 
 		list = @db.execute("SELECT * FROM products ORDER BY type ASC;")
